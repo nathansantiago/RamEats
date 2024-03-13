1e3c20a562889ab9c5a81eb236b1d52e5f11ae8c
Nathan Santiago <santiagonathan9254@gmail.com>
2024-03-13 13:22:33 -0400

    Create Navbar

--- RamEatsApp/src/components/layout/index.jsx
+++ RamEatsApp/src/components/layout/index.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './index.scss'
+import Navbar from '../navbar'
 
 const Layout = () => {
     return (
         <div className='App'>
             <Outlet />
+            <Navbar />
         </div>
     )
 }
--- RamEatsApp/src/components/navbar/index.jsx
+++ RamEatsApp/src/components/navbar/index.jsx
@@ -1,8 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faHome } from '@fortawesome/free-solid-svg-icons';
 import './index.scss'
 
 const Navbar = () => {
+
     return (
-        <></>
+        <>
+            <div className='bottom-nav'>
+                <nav>
+                    <NavLink
+                        exact="true"
+                        activeclassname="active"
+                        to="/"
+                    >
+                        <FontAwesomeIcon icon={faHome} color="#000" />
+                    </NavLink>
+                    <NavLink
+                        activeclassname="active"
+                        to="/settings"
+                    >
+                        <FontAwesomeIcon icon={faCog} color="#000" />
+                    </NavLink>
+                </nav>
+            </div>
+        </>
     )
 }
 
