adebc593f411b7102d379db1b03e1906af1979b4
Nathan Santiago <santiagonathan9254@gmail.com>
2024-03-14 12:32:29 -0400

    Style login page.

--- RamEatsApp/src/components/login/index.jsx
+++ RamEatsApp/src/components/login/index.jsx
@@ -4,15 +4,19 @@ import logo from '../../assets/RamEatsLogo.svg'
 const Login = () => {
     return (
         <>
-            <img src={logo} alt='Logo' height={'50px'}/>
-            <h1>RamEats</h1>
-            <form> {/** action defines location to send data method describes http method to use */}
-                <label for='email'>Email:</label>
-                <input type='email' name='email' id='email' required />
-                <label for='password'>Password:</label>
-                <input type='password' name='password' id='password' required />
-                <input type='submit' value='Login'/>
-            </form>
+            <div className='login-page'>
+                <ul>
+                    <img src={logo} alt='Logo' height={'50px'}/> 
+                    <h1>RamEats</h1> 
+                    <form> {/** action defines location to send data method describes http method to use */}
+                        <label for='email'>Email:</label> <br/>
+                        <input type='email' name='email' id='email' required /> <br/>
+                        <label for='password'>Password:</label> <br/>
+                        <input type='password' name='password' id='password' required /> <br/>
+                        <button>Login</button>
+                    </form>
+                </ul>
+            </div>
         </>
     );
 }
