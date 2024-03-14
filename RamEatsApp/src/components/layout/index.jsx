dced047c2c6a3a6c4af3945c8cc372f29fbd492b
Nathan Santiago <santiagonathan9254@gmail.com>
2024-03-14 10:38:53 -0400

    Create Topbar

--- RamEatsApp/src/components/layout/index.jsx
+++ RamEatsApp/src/components/layout/index.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './index.scss'
 import Navbar from '../navbar'
+import Topbar from '../topbar'
 
 const Layout = () => {
     return (
         <div className='App'>
+            <Topbar />
             <Outlet />
             <Navbar />
         </div>
