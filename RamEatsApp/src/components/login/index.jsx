5a2855aa015a3b20d19f71ad14749918efbe88a2
Nathan Santiago <santiagonathan9254@gmail.com>
2024-03-15 11:04:39 -0400

    Style login

--- RamEatsApp/src/components/login/index.jsx
+++ RamEatsApp/src/components/login/index.jsx
@@ -9,13 +9,16 @@ const Login = () => {
                     <img src={logo} alt='Logo' height={'50px'}/> 
                     <h1>RamEats</h1> 
                     <form> {/** action defines location to send data method describes http method to use */}
-                        <label for='email'>Email:</label> <br/>
+                        <label for='email'>Email</label> <br/>
                         <input type='email' name='email' id='email' required /> <br/>
-                        <label for='password'>Password:</label> <br/>
+                        <label for='password'>Password</label> <br/>
                         <input type='password' name='password' id='password' required /> <br/>
-                        <button>Login</button>
+                        <button className='login'>Login</button>
+                        <a className='newPass'>Forgot password?</a>
                     </form>
                 </ul>
+
+                <p>Don't have an account? <a className='signUp'>Sign Up</a></p>
             </div>
         </>
     );
